de9d44d7b8bccf46c151863e70715aa2593194bc
andresperezfc <felipe3apc@gmail.com>
2020-02-27 15:03:12 -0500

    28 - Comment Resource

--- app/Http/Controllers/StatusCommentsController.php
+++ app/Http/Controllers/StatusCommentsController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use App\Http\Resources\CommentResource;
 
 class StatusCommentsController extends Controller
 {
     public function store(Status $status){
 
-        Comment::create([
+        $comment = Comment::create([
             'user_id' => auth()->id(),
             'status_id' => $status->id,
             'body' => request('body')
         ]);
+        return CommentResource::make($comment);
     }
 }
